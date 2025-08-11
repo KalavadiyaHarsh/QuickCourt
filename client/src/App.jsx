69274a4c39eb3747ecbfa97a5525824a58222294
harsh <harshkalavadiya192@gmail.com>
2025-08-11 11:00:11 +0530

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div className='bg-red-500 '>
-      hill
+    <div className='bg-red-500 w-full h-screen flex items-center justify-center'>
+      hill  harsh
     </div>
   );
 }
